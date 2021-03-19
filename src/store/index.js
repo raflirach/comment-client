a374419676a8cae3c58d2293d5688d4c54b94080
raflirach <rafli060395@gmail.com>
2021-03-19 10:28:17 +0700

    add: fetch comment action

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '../api/axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    comments: []
   },
   mutations: {
+    insertComment (state, payload) {
+      state.comments = payload
+    }
   },
   actions: {
-  },
-  modules: {
+    async fetchComment ({ commit }, payload) {
+      try {
+        const { data } = await axios({
+          url: '/comments'
+        })
+        commit('insertComment', data)
+      } catch (err) {
+        console.log(err.response.data)
+      }
+    }
   }
 })
