c8ff484807191ed1d88ab48da2ae4a8d17a4e774
raflirach <rafli060395@gmail.com>
2021-03-19 11:31:10 +0700

    add: form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="w-full h-full flex justify-center bg-gray-100">
     <div class="w-3/4 border bg-white flex flex-col px-14 py-8">
-      <comment v-for="comment in comments" :key="comment.id" :comment="comment" />
+      <Form :field="'comment'"/>
+      <Comment v-for="comment in comments" :key="comment.id" :comment="comment" />
     </div>
   </div>
 </template>
 
 <script>
 import Comment from '../components/Comment.vue'
+import Form from '../components/Form.vue'
 // @ is an alias to /src
 
 export default {
-  components: { Comment },
+  components: { Comment, Form },
   name: 'Home',
   methods: {
     fetchComment () {
